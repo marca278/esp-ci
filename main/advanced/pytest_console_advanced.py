b0a17192c8aef4c689194c7fb00506ae56a9ad96
marc <marc.adler@helios-embedded.de>
2026-05-13 21:57:55 +0200

    init

---
+++ main/advanced/pytest_console_advanced.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2021-2025 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: CC0-1.0
+from time import sleep
+
+import pytest
+from pytest_embedded import Dut
+from pytest_embedded_idf.utils import idf_parametrize
+from pytest_embedded_qemu.dut import QemuDut
+
+
+def _verify_console_advanced(dut: Dut) -> None:
+    sleep(2)  # Some time for the OS to enumerate our USB device
+
+    # Wait until the console prompt appears
+    dut.expect(r'esp32(?:c3)?> ')
+
+    # Write CLI command "version"
+    dut.write('version')
+    sleep(0.5)
+
+    # Check if following strings are present in the "version" command output
+    dut.expect('IDF Version')
+    dut.expect('Chip info')
+
+
+@pytest.mark.generic
+@idf_parametrize('target', ['esp32', 'esp32c3'], indirect=['target'])
+def test_console_advanced(dut: Dut) -> None:
+    _verify_console_advanced(dut)
+
+
+@pytest.mark.qemu
+@pytest.mark.parametrize('embedded_services', ['idf,qemu'], indirect=True)
+@idf_parametrize('target', ['esp32'], indirect=['target'])
+def test_console_advanced_qemu(dut: QemuDut) -> None:
+    _verify_console_advanced(dut)
